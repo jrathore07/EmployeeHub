395ffd5e342c6fbf6bda05e06fafd635fe4486a1
Jay <jaydeeprathore69@gmail.com>
2026-07-24 13:21:47 +0530

    Added Angular frontend and updated backend

--- src/main/java/com/firstangular/controller/EmployeeController.java
+++ src/main/java/com/firstangular/controller/EmployeeController.java
@@ -3,11 +3,10 @@ package com.firstangular.controller;
 
 import com.firstangular.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.firstangular.repository.EmployeeRepository;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/vi/")
 public class EmployeeController {
@@ -18,6 +17,11 @@ public class EmployeeController {
     @GetMapping("/employees")
     public Iterable<Employee> findAll() {
         return employeeRepository.findAll();
+    }
+
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
     }
 
 }
