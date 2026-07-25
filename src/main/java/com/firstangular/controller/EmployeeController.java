425ca6ca76f415689964078ba9ed20b54ae73322
Jay <jaydeeprathore69@gmail.com>
2026-07-25 10:51:49 +0530

    Added employee update, delete and view features in backend and frontend

--- src/main/java/com/firstangular/controller/EmployeeController.java
+++ src/main/java/com/firstangular/controller/EmployeeController.java
@@ -1,10 +1,15 @@
 package com.firstangular.controller;
 
 
+import com.firstangular.exception.ResourceNotFoundException;
 import com.firstangular.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.firstangular.repository.EmployeeRepository;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -22,6 +27,40 @@ public class EmployeeController {
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
+    }
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> findEmployeeById(@PathVariable long id) {
+
+        Employee employee= employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found with id " + id));
+        return ResponseEntity.ok(employee);
+    }
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id,@RequestBody Employee employeeDetails) {
+
+        Employee employee= employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found with id " + id));
+
+        employee.setFirstName(employeeDetails.getFirstName());
+        employee.setLastName(employeeDetails.getLastName());
+        employee.setEmail(employeeDetails.getEmail());
+
+        Employee updatedEmployee=employeeRepository.save(employee);
+        return ResponseEntity.ok(updatedEmployee);
+
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<Map<String,Boolean>> deleteEmployee(@PathVariable long id) {
+
+        Employee employee= employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found with id " + id));
+
+        employeeRepository.deleteById(employee.getId());
+        Map<String,Boolean> response=new HashMap<>();
+        response.put("deleted",true);
+        return ResponseEntity.ok(response);
+
     }
 
 }
